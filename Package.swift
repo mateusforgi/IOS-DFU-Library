367a99057244a06406295e8639d1b6dc203b105d
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2023-02-07 17:33:25 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,8 @@
+// swift-tools-version:5.5
+ //
+ // The `swift-tools-version` declares the minimum version of Swift required to
+ // build this package. Do not remove it.
+
 import PackageDescription
 
 let package = Package(
