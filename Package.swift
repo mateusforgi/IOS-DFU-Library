3669e835953638536596347d113fc536bed810fc
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2023-02-24 11:31:30 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,41 +1,22 @@
-// swift-tools-version:5.5
-//
-// The `swift-tools-version` declares the minimum version of Swift required to
-// build this package. Do not remove it.
-
 import PackageDescription
 
 let package = Package(
-  name: "NordicDFU",
-  platforms: [
-    .macOS(.v10_14),
-    .iOS(.v9),
-    .watchOS(.v4),
-    .tvOS(.v11)
-  ],
-  products: [
-    .library(name: "NordicDFU", type: .dynamic, targets: ["NordicDFU"]),
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/mateusforgi/ZIPFoundation",
-      revision: "e71588939c0875c428374d9b064b3ea801fe95a0"
-    )
-  ],
-  targets: [
-    .target(
-      name: "NordicDFU",
-      dependencies: [.productItem(name: "ZIPFoundationDynamic",
-                                  package: "ZIPFoundation")],
-      path: "iOSDFULibrary/Classes/"
-    ),
-    // FIXME: Exclude this target for `watchOS` Simulator, because it fails to
-    // compile in Xcode.
-    .testTarget(
-      name: "Hex2BinConverterTests",
-      dependencies: ["NordicDFU"],
-      path: "Example/Tests/"
-    )
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "NordicDFUDynamic",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "NordicDFUDynamic",
+            type: .dynamic,
+            targets: ["NordicDFUDynamic"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", .exact("4.12.0")),
+    ],
+    targets: [
+        .target(
+            name: "NordicDFUDynamic",
+            dependencies: [.product(name: "NordicDFU", package: "IOS-DFU-Library")]
+        ),
+    ]
 )
